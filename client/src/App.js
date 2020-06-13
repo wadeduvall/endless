b06a3d8d09b7121c47b0a7df2707af9c342eca0d
Wade Duvall <ackthet@gmail.com>
2020-06-13 23:19:35 +0000

    Refactor app to have state.

--- client/src/App.js
+++ client/src/App.js
@@ -20,74 +20,89 @@ import {
   MapComponent
 } from '@terrestris/react-geo';
 
-const layer = new LayerTile({
-  source: new XYZSource({
-    url: 'http://192.168.1.64/tile/{z}/{x}/{y}.png'
-  })
-});
-
-const vectorSource = new VectorSource();
-
-const vectorLayer = new VectorLayer({
-  source: vectorSource
-});
-
-const center = fromLonLat([-77.1878, 38.8646])
-
-const map = new Map({
-  view: new View({
-    center: center,
-    zoom: 16,
-  }),
-  layers: [
-    layer,
-    vectorLayer,
-  ]
-});
-
-map.on('click', function(evt) {
-  const feature = new Feature({
-    type: 'Point',
-    geometry: new Point(evt.coordinate)
+function makeMap(props) {
+  const layer = new LayerTile({
+    source: new XYZSource({
+      url: 'http://192.168.1.75/tile/{z}/{x}/{y}.png'
+    })
   });
-  vectorSource.addFeature(feature);
-  // if features > 2, query the router
-  const features = vectorSource.getFeatures();
-  if (features.length > 1) {
-    const clickCoords = features.filter(ftr => {
-      return ftr.get('type') == 'Point';
-    }).map(ftr => {
-      return toLonLat(ftr.getGeometry().getCoordinates());
-    });
-    const request = clickCoords.map(pos => {
-      return pos[0].toString() + ',' + pos[1];
-    }).join(';');
-    fetch('//192.168.1.64/route/v1/walking/' + request).then(
-      r => {
-        return r.json();
-      }).then(json => {
-      const polyline = json.routes[0].geometry;
-      const route = new Polyline().readGeometry(polyline, {
-        dataProjetion: 'EPSG:4326',
-        featureProjection: 'EPSG:3857'
-      });
-      const feature = new Feature({
-        type: 'LineString',
-        geometry: route
-      });
-      vectorSource.addFeature(feature);
-    });
-  }
-});
 
-function App() {
-  return (
-    <div className="App">
-      <MapComponent
-        map={map}
-      />
-    </div>
-  );
+  const vectorSource = new VectorSource();
+
+  const vectorLayer = new VectorLayer({
+    source: vectorSource
+  });
+
+  const center = fromLonLat([-77.1878, 38.8646])
+
+  const map = new Map({
+    view: new View({
+      center: center,
+      zoom: 16,
+    }),
+    layers: [
+      layer,
+      vectorLayer,
+    ]
+  });
+
+  map.on('click', function(evt) {
+    const feature = new Feature({
+      type: 'Point',
+      geometry: new Point(evt.coordinate)
+    });
+    vectorSource.addFeature(feature);
+    // if features > 2, query the router
+    const features = vectorSource.getFeatures();
+    if (features.length > 1) {
+      const clickCoords = features.filter(ftr => {
+        return ftr.get('type') == 'Point';
+      }).map(ftr => {
+        return toLonLat(ftr.getGeometry().getCoordinates());
+      });
+      const request = clickCoords.map(pos => {
+        return pos[0].toString() + ',' + pos[1];
+      }).join(';');
+      fetch('//192.168.1.75/route/v1/walking/' + request + '?geometries=polyline6').then(
+        r => {
+          return r.json();
+        }).then(json => {
+          const polyline = json.routes[0].geometry;
+          const route = new Polyline({
+            factor: 1e6,
+          }).readGeometry(polyline, {
+            dataProjetion: 'EPSG:4326',
+            featureProjection: 'EPSG:3857'
+          });
+          const feature = new Feature({
+            type: 'LineString',
+            geometry: route
+          });
+          vectorSource.addFeature(feature);
+        });
+    }
+  });
+
+  return map;
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      map: makeMap()
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <MapComponent
+          map={this.state.map}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
